038bd52e88613a9141a506e60da4b74fb7e83fdf
RickWeber <Rick.Weber05@gmail.com>
2021-05-29 15:21:52 -0400

    have clustering start with centers from prior clustering.

--- R/functions.R
+++ R/functions.R
@@ -10,38 +10,6 @@ gower <- function(df){
   }) %>% unlist %>% matrix(.,ncol = nrow(df))
 } 
 
-# Let's take each year, assign cluster memberships, then use those values to 
-# cluster the next year. That is, cluster assignment is a function of this 
-# this year's values and next year's cluster membership.
-cluster_back <- function(df, k){
-  yrs <- grab_yrs(df)
-  df$cluster <- integer(nrow(df))
-  df <- cluster_yr(df,yrs[1],k)
-  for(i in 2:length(yrs)){
-    y <- yrs[i]
-    df <- cluster_yr_up(df,y,k)
-  }
-  return(df)
-} ; cluster_back(df,4)
-
-cluster_yr <- function(df,yr,k){
-  df <- df %>%
-    filter(Year == yr)
-  df[df$Year == yr,'cluster'] <- (df %>% gower %>% kmeans(k))$cluster
-  return(df)
-}
-
-cluster_yr_up <- function(df,yr,k){
-  df1 <- df %>% filter(Year == yr)
-  # previous year's data should have cluster membership
-  yr2 <- two_yrs(df,yr,F) %>% setdiff(yr)
-  df2 <- df %>% filter(Year == yr2)
-  df3 <- df2 %>% select(-contains("Area"),-Year,-contains("Summary")) %>% full_join(df1)
-  df[df$Year == yr,'cluster'] <- (df3 %>% gower %>% kmeans(k))$cluster
-  return(df)
-}
-
-
 # Convenience function for grabbing the years from the dataset.
 grab_yrs <- function(df){
   unique(df$Year) %>% sort(decreasing = TRUE)
@@ -53,8 +21,49 @@ two_yrs <- function(df,yr,prev = TRUE){
   yr_j <- ifelse(prev,yr_i-1,yr_i+1)
   return(yrs[c(yr_i,yr_j)])
 }
+# cluster a single year based on underlying data alone.
+cluster_yr <- function(df,yr,k){
+  kdata <- df %>% filter(Year == yr) %>% gower %>% kmeans(k)
+  df[df$Year == yr,'cluster'] <- kdata$cluster
+  return(list(df=df,kdata=kdata))
+}
+# use another year's clustering outcome to add information to this years
+# then cluster. 
+cluster2cluster <- function(df,yr,kdata){
+  df1 <- df %>% filter(Year == yr) %>% mutate(cluster = kdata$cluster)
+  df1 %>% gower %>% kmeans(centers = kdata$centers)
+} 
+# Cluster the latest year then use the results to cluster each 
+# prior year 
+cluster_back <- function(df, k){
+  yrs <- grab_yrs(df)
+  cluster1 <- cluster_yr(df,yrs[1],k)
+  kdata <- cluster1$kdata
+  df <- cluster1$df
+  for(i in 2:length(yrs)){
+    y <- yrs[i]
+    kdata <- cluster2cluster(df,y,kdata)
+    df <- df[Year == y,'cluster'] <- kdata$cluster
+  }
+  return(df)
+}
+# Same as above, but starting earlier in time and walking forward.
+cluster_fore <- function(df, k){
+  yrs <- grab_yrs(df) %>% rev
+  cluster1 <- cluster_yr(df,yrs[1],k)
+  kdata <- cluster1$kdata
+  df <- cluster1$df
+  for(i in 2:length(yrs)){
+    y <- yrs[i]
+    kdata <- cluster2cluster(df,y,kdata)
+    df <- df[Year == y,'cluster'] <- kdata$cluster
+  }
+  return(df)
+}
+
 
 # Standardize column headers that look like 'year', 'country', or 'iso'
+# These functions will make the library more general purpose.
 yearify_cols <- function(df){
   FALSE
 }
