fdd11d8d14ad0525f7a9a91689dc1b8199f13a06
RickWeber <Rick.Weber05@gmail.com>
2021-05-29 11:53:52 -0400

    implement convenience functions for dealing with years with some gaps

--- R/functions.R
+++ R/functions.R
@@ -14,8 +14,10 @@ gower <- function(df){
 # cluster the next year. That is, cluster assignment is a function of this 
 # this year's values and next year's cluster membership.
 
+
+
 cluster_back <- function(df, k){
-  yrs <- unique(df$Year) %>% sort(decreasing = TRUE)
+  yrs <- grab_yrs(df)
   first_yr <- max(yrs) # first year to cluster on, not first in the data
   last_yr <- min(yrs)
   df$cluster <- NA
@@ -37,16 +39,38 @@ cluster_back <- function(df, k){
     }
   }
 }
-# cluster backwards through time
-cluster_back <- function(df, k){
-  yrs <- unique(df$Year) %>% sort(decreasing = TRUE)
-  df$cluster <- NA
-  for(i in 1:length(yrs)){
-    y <- yrs[i]
-    y1 <- yrs[i + 1]
-    x <- df %>% filter(Year == y)
-    d <- gower(x)
-    c <- kmeans(d,k)$cluster
-    df[df$Year == y1,'cluster'] <- c
-  }
-} ; cluster_back(df,4)
+cluster_back(df,4)
+
+cluster_yr <- function(df,yr,k){
+  clusters <- df %>%  
+    filter(Year == yr) %>%
+    gower %>%
+    kmeans(k)$cluster
+  df[df$Year == yr,'cluster'] <- clusters
+  return(df)
+}
+
+cluster_yr_back <- function(df,yr,k){
+  
+  yr2 <- yrs[2]
+  df1 <- df[df$Year == yr,]
+  df2 <- df[df$Year == yr2]
+}
+
+
+# Convenience function for grabbing the years from the dataset.
+grab_yrs <- function(df){
+  unique(df$Year) %>% sort(decreasing = TRUE)
+}
+# Get two adjacent years
+two_yrs <- function(df,yr,prev = TRUE){
+  yrs <- grab_yrs(df)
+  yr_i <- match(yr,yrs)
+  yr_j <- ifelse(prev,yr_i-1,yr_i+1)
+  return(yrs[c(yr_i,yr_j)])
+}
+
+# Standardize column headers that look like 'year', 'country', or 'iso'
+yearify_cols <- function(df){
+  FALSE
+}
