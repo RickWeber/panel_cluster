9e1767bdd723c89e881f3068ee64131b76cf87be
Rick Weber <Rick.Weber05@gmail.com>
2021-08-09 09:12:06 -0400

    clean up folder

--- R/03_functions.R
+++ R/03_functions.R
@@ -2,122 +2,169 @@ rm(list = ls())
 source("01_libraries.R")
 source("02_import_data.R") ; df <- efw_data_panel
 
+### Widen data
+efw_widen <- function(df, vars=paste0("efw",1:5)){
+  df %>%
+    select(-overall) %>%
+    pivot_wider(names_from = "year",
+                values_from = vars)
+}
+### Widen then cluster
+cluster_wide <- function(df, k = 4){
+  out <- cbind((df %>%
+                  efw_widen),
+               (df %>%
+                  efw_widen %>% 
+                  select(contains("efw")) %>%
+                  scale %>%
+                  dist %>%
+                  kmeans(4))$cluster) %>%
+    as_tibble
+  colnames(out)[ncol(out)] <- "cluster"
+  out
+}
 
-# Calculate a distance matrix between each row.
+### Add principal components on to a dataframe
+add_pca_efw <- function(df,components = 2){
+  # add first two principal components onto a data frame
+  pca <- df %>%
+    select(contains("efw")) %>%
+    na.omit %>%
+    prcomp
+  pca <- pca$x[,paste0("PC",1:components)]
+  cbind(na.omit(df),pca) %>% as_tibble
+}
+
+### Calculate a distance matrix between each row using Gower distance.
 gower <- function(df){
   map(1:nrow(df),function(x){
     gower_dist(df[x,],df)
   }) %>% unlist %>% matrix(.,ncol = nrow(df))
 } 
 
-# update defaults for kmeans
-# kmeans <- function(x, centers = 4, iter.max = 20L, nstart = 1L){
-#   kmeans(x, centers = centers, iter.max = iter.max, nstart = nstart)
-# }
+### Year-wise clustering
+cluster_by_year <- function(df, k = 4){
+  df %>%
+    group_by(year) %>%
+    nest %>%
+    mutate(cluster_object = map(data,function(d){
+      d %>% select(contains("efw")) %>% na.omit %>% scale %>% kmeans(k)
+    })) %>%
+    mutate(clustered_data = map2(data,cluster_object,function(d,c){
+      cluster = c$cluster
+      cbind(d %>% na.omit,cluster) %>% as_tibble
+    }))
+} # ; df %>% cluster_by_year
 
-##### June 1 work
-# Focus on aligning clusters.
-# If I've got two datasets with clusters that aren't already identical, 
-# then I want to either have centroids, or find sample centroids, then
-# use those centroids to re-align then redefine the clusters.
-align_clusters2 <- function(clustered_data1, clustered_data2){
-  df1 <- clustered_data1 ; df2 <- clustered_data2
-  df3 <- full_join(df1, df2,
-                   by = c("Countries", "cluster"),
-                   suffix = c(".1",".2"))
-  # Already aligned case:
-  if(df3$cluster.1 == df3$cluster.2){
-    return(full_join(df1,df2))
+### Cluster alignment
+
+# Cluster alignment
+
+cluster_centers <- function(df){
+  # Convenience function
+  df %>% 
+    group_by(cluster) %>%
+    summarize_if(is.double,funs(mean(.,na.rm = T)))
+}
+
+cluster_center_distances <- function(df0,df1){
+  nclusters = unique(df0$cluster) %>% length
+  centers0 <- df0 %>%
+    select(cluster,contains("efw")) %>%
+    cluster_centers
+  centers1 <- df1 %>%
+    select(cluster,contains("efw")) %>%
+    cluster_centers
+  out <- map(1:nclusters,
+             function(x){
+               c0 <- centers0 %>%
+                 filter(cluster == x) %>%
+                 select(-cluster)
+               map_dbl((1:nclusters) + nclusters,
+                       function(x){
+                         c1 <- centers1 %>%
+                           filter(cluster == x) %>%
+                           select(-cluster)
+                         (c0 - c1)^2 %>% 
+                           sum %>%
+                           sqrt
+                       })
+             }) %>% 
+    unlist %>%
+    matrix(ncol = nclusters)
+  rownames(out) <- (1:nclusters) + 4
+  colnames(out) <- 1:nclusters
+  out
+}
+
+evaluate_mapping <- function(mapping, distances){
+  # rearrange the distances matrix based on the mapping
+  diag(distances[mapping,])^2 %>% 
+    sum
+}
+
+choose_mapping <- function(distances){
+  possible_mappings <- permutations(ncol(distances),ncol(distances))
+  nmappings <- nrow(possible_mappings)
+  out <- possible_mappings[1,]
+  best_score <- evaluate_mapping(out, distances)
+  for (m in 1:nmappings){
+    current_score <- evaluate_mapping(possible_mappings[m,],distances)
+    if (current_score < best_score){
+      out <- possible_mappings[m,]
+      best_score <- current_score
+    }
   }
-  # Else, find centroids for df1$cluster
-  centroids.1 <- df1 %>% find_centroids
+  out
 }
 
-find_centroids(df,cluster_var = "cluster"){
-  df %>% group_by(cluster_var) %>%
-    summarize_if(is.numeric,funs(mean(.,na.rm=TRUE)))
-}
-
-df_clustered <- df %>% 
-  group_by(Year) %>% 
-  nest %>%
-  mutate(dist = map(data,gower),
-         k = map(dist,function(x){
-           kmeans(x,4)
-         })) # from here I need to extract cluster membership.
-
-extract_cluster <- function(kdata){
-  # pull cluster membership from a nested
-}
-align_clusters <- function(cluster_var,group_var,first=TRUE){}
-##### Work before June 1
-# Convenience function for grabbing the years from the dataset.
-grab_yrs <- function(df){
-  unique(df$Year) %>% sort(decreasing = TRUE)
-}
-# Get two adjacent years
-two_yrs <- function(df,yr,prev = TRUE){
-  yrs <- grab_yrs(df)
-  yr_i <- match(yr,yrs)
-  yr_j <- ifelse(prev,yr_i-1,yr_i+1)
-  return(yrs[c(yr_i,yr_j)])
-}
-# cluster a single year based on underlying data alone.
-cluster_yr <- function(df,yr,k){
-  kdata <- df %>% filter(Year == yr) %>% gower %>% kmeans(k, nstart = 10)
-  df[df$Year == yr,'cluster'] <- kdata$cluster
-  return(list(df=df,kdata=kdata))
-}
-# use another year's clustering outcome to add information to this years
-# then cluster. 
-cluster2cluster <- function(df,yr,kdata){
-  df <- df %>% filter(Year == yr) 
-  df[df$Year == yr,'cluster'] <- kdata$cluster
-  df %>% gower %>% kmeans(centers = kdata$centers)
-} 
-# Cluster the latest year then use the results to cluster each 
-# prior year 
-cluster_back <- function(df, k){
-  yrs <- grab_yrs(df)
-  cluster1 <- cluster_yr(df,yrs[1],k)
-  kdata <- cluster1$kdata
-  df <- cluster1$df
-  for(i in 2:length(yrs)){ # replace with seq_along?
-    y <- yrs[i]
-    kdata <- cluster2cluster(df,y,kdata)
-    df <- df[df$Year == y,'cluster'] <- kdata$cluster
+relabel <- function(df,mapping){
+  # relabel a data frame with shifted labels according to a provided mapping
+  nclusters = unique(df$cluster) %>% length
+  for(c in 1:nclusters){
+    df <- df %>%
+      mutate(cluster = ifelse(cluster == (c + nclusters),
+                              mapping[c],
+                              cluster))
   }
-  return(df)
+  df
 }
-# Same as above, but starting earlier in time and walking forward.
-cluster_fore <- function(df, k){
-  yrs <- grab_yrs(df) %>% rev
-  cluster1 <- cluster_yr(df,yrs[1],k)
-  kdata <- cluster1$kdata
-  df <- cluster1$df
-  for(i in 2:length(yrs)){
-    y <- yrs[i]
-    kdata <- cluster2cluster(df,y,kdata)
-    df <- df[df$Year == y,'cluster'] <- kdata$cluster
+
+cluster_alignment <- function(clustered_panel_df){
+  out <- clustered_panel_df
+  yrs <- unique(out$year) %>% sort
+  clusters <- unique(out$cluster) %>% sort
+  nclusters <- length(clusters)
+  for(y in setdiff(yrs,first(yrs))){
+    previous_yr <- yrs[yrs < y] %>% last
+    # line up y's cluster labels with the previous year's
+    df0 <- out %>% filter(year == previous_yr)
+    df1 <- out %>% filter(year == y) %>%
+      mutate(cluster = cluster + nclusters) # avoid over-writing
+    # Calculate distances between cluster centers
+    distances <- cluster_center_distances(df0,df1)
+    # choose a mapping to minimize total distance between centers
+    mapping <- choose_mapping(distances)
+    # Let's minimize the sum of squared differences
+    # I can represent the mapping with a permutation of 1:nclusters
+    # that maps each of the new clusters to the original cluster labels
+    # I want to choose the mapping that minimizes the distance between 
+    # centers
+    mapping <- sample(1:nclusters) # random mapping for testing
+    df1 <- df1 %>%
+      relabel(mapping)
+    # Now I can overwrite the data I started with
+    out[out$year == y,"cluster"] <- df1$cluster
   }
-  return(df)
+  out
 }
 
-# debugging 
-t <- cluster_yr(df,2018,4)
-t1 <- cluster2cluster(df, 2017, t$kdata)
-cbind(t1$cluster, t$df$cluster) %>% head(length(t1$cluster))
-# looks like it's doing what it's supposed to...
-# but I'm getting this error:
-#
-# Error in UseMethod("filter") : 
-#   no applicable method for 'filter' applied to an object of class "c('integer', 'numeric')"
-# In addition: There were 50 or more warnings (use warnings() to see the first 50)
-#
-# when I run: t <- cluster_fore(df,4)
+t1 <- df %>% cluster_by_year %>% select(-cluster_object,-data) %>% unnest()
+t2 <- t1 %>% cluster_alignment
 
-# Standardize column headers that look like 'year', 'country', or 'iso'
-# These functions will make the library more general purpose.
-yearify_cols <- function(df){
-  FALSE
-}
+t2 %>%
+  ggplot(aes(year,country)) +
+  geom_tile(aes(fill = as.factor(cluster))) + 
+  theme_minimal() +
+  scale_fill_brewer(type = "qual",
+                    palette = "Set1")
