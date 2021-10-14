1b601988ccf015c17e1571cb47df1cd11d456f95
RickWeber <Rick.Weber05@gmail.com>
2021-10-14 10:20:00 -0400

    backup before conference

--- R/03_functions.R
+++ R/03_functions.R
@@ -47,6 +47,8 @@ add_pca_efw <- function(df,components = 2){
   pca <- pca$x[,paste0("PC",1:components)]
   cbind(na.omit(df),pca) %>% as_tibble
 }
+# Note to self: replace this with https://www.rdocumentation.org/packages/factoextra/versions/1.0.7/topics/fviz_cluster
+# and related 
 
 ### Calculate a distance matrix between each row using Gower distance.
 gower <- function(df){
@@ -106,8 +108,73 @@ chained_clustering_by_year <- function(df, k = 4){
   return(out_df)
 }
 
-###
+## realign cluster labels
+### find cluster centers
+cluster_centers <- function(clustered_df){
+  # Convenience function
+  clustered_df %>% ungroup %>%
+    group_by(cluster) %>%
+    # should explicitly select data columns
+    # or maybe I just select out columns I don't want before
+    # running data through this function
+    summarize_if(is.double,funs(mean(.,na.rm = T))) %>%
+    ungroup %>%
+    arrange(cluster) %>%
+    select(-cluster)
+}
 
+### measure distances between cluster centers
+center_distances <- function(base_centers, next_centers){
+  # Note:
+  # element [i,j] of the output shows the distance between
+  # base_center[i,] and next_center[j,]
+  base_k <- nrow(base_centers)
+  next_k <- nrow(next_centers)
+  if (base_k != next_k) return("ERROR")
+  k <- base_k
+  map(1:k,
+      function(i){
+        map_dbl(1:k,
+                function(j){
+                  # get distance between center j from next and center i from base
+                  rbind(base_centers[i,],
+                        next_centers[j,]) %>%
+                    dist
+                  # put it into element i,j of distances
+                })
+      }) %>% unlist %>% 
+    matrix(nrow = k) %>% t 
+}
+
+### sum of square distances
+evaluate_mapping <- function(mapping, distances){
+  diag(distances[mapping,])^2 %>% sum
+}
+
+### choose a mapping to minimize distances
+choose_mapping <- function(distances){
+  possible_mappings <- permutations(ncol(distances),ncol(distances))
+  nmappings <- nrow(possible_mappings)
+  scores <- map_dbl(1:nmappings,
+                    function(m){
+                      evaluate_mapping(possible_mappings[m,],distances)
+                    })
+  possible_mappings[which.min(scores),]
+}
+
+### relabel clusters based on a given mapping
+relabel <- function(df, mapping){
+  k <- length(mapping)
+  df <- df %>%
+    mutate(cluster = cluster + k)
+  for (c in 1:k) {
+    df <- df %>%
+      mutate(cluster = ifelse(cluster == (c + k),
+                              mapping[c],
+                              cluster))
+  }
+  df
+}
 
 
 
